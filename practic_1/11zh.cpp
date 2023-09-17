7da9cc08308ed6ff624a3ac360aba8be1f8c57f4
ZaninV3 <zanin03.03@mail.ru>
2023-09-17 15:39:56 +0900

    Выполнено задание 2 из практики 1
    Мой вариант на Программировании в I курсе был 16-й, поэтому взял задание 11Ж из [задачника](https://ivtipm.github.io/Programming/Files/spisocall.htm).
    
    Для удобства прикрепил формулы 11zh.png, по которым должны получиться значения a и b.
    
    Полученная программа скомпилирована.

---
+++ practic_1/11zh.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Задание 11ж
+// Даны x, y, z. Вычислить a и b (см. 11zh.png)
+
+// Возвращает значение a
+// Вернет nullptr, если введенные значения приводят к ошибке
+float* getA(float x, float y, float z) {
+    // Первая проверка a. Если знаменатель равен 0, то деление невозможно
+    if ((z + x * x / 4) != 0) {
+        float first = y - sqrt(abs(x));
+        float second = x - 4 / (z + pow(x, 2) / 4);
+
+        // Вторая проверка a. Если один из множителей равен 0,
+        // то возведение под натур. логарифм невозможно.ы
+        if (first != 0 and second != 0) {
+            float* result;
+            *result = log(abs(first * second));
+            return result;
+        }
+        else return nullptr;
+    }
+    else return nullptr;
+}
+
+// Возвращает значение b
+// Делать проверки, подобно в getA() не имеет смысла
+float* getB(float x) {
+    float* result;
+    // Для удобства 3! и 5! заранее подсчитаны 6 и 120 соответственно
+    float res = x - (pow(x, 2) / 6) + (pow(x, 5) / 120);
+    result = &res;
+    return result;
+}
+
+// Обработка и вывод результатов работы функций getA и getB
+void printValue(float* result) {
+    // Если результат -- нулевой указатель, то сообщаем об ошибке
+    if (result == nullptr) cout << "error" << endl;
+    else cout << *result << endl;  // Иначе выводим результат
+}
+
+
+int main() {
+    // Вводим значения для подсчета
+    float x;
+    cout << "x = ";
+    cin >> x;
+
+    float y;
+    cout << "y = ";
+    cin >> y;
+
+    float z;
+    cout << "z = ";
+    cin >> z;
+
+    // Вывод результатов
+    cout << "-----------" << endl;
+    cout << "a = ";
+    printValue(getA(x, y, z));
+    cout << "b = ";
+    printValue(getB(x));
+}
