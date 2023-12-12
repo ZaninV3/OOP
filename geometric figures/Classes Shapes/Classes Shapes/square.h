e2d645e6aea43a728d01ae548b43c0daf4c1a895
ZaninV3 <zanin03.03@mail.ru>
2023-12-12 21:32:35 +0900

    Полностью дописан класс Square
    Разделил объявление и реализацию методов. Также Дописал комментарий для getPerimeter()

--- geometric figures/Classes Shapes/Classes Shapes/square.h
+++ geometric figures/Classes Shapes/Classes Shapes/square.h
@@ -13,45 +13,72 @@
 // bool setSide(float side) -- Меняет значение длины и возвращает true, если замена прошла успешно
 class Square {
 private:
+	// Длина стороны квадрата
 	float side;
 public:
 	// Конструктор 
 	// Инициализирует значение side, если указанное значение больше или равно 0
 	// Иначе задает по умолчанию 0
-	Square(float pside = 0) {
-		// Если pside меньше 0, то задаем значение по умолчанию
-		if (pside < 0) this->side = 0;
-		// Иначе параметр указан корректно
-		else this->side = pside;
-	}
+	Square(float pside = 0);
 
 	// Возвращает значение стороны
-	float getSide() const {
-		return this->side;
-	}
+	float getSide() const;
 	// const здесь означает, что у функции ограничение на использование 
 	// То есть если функция объявлена с константным указателем this,
 	// то в теле функции запрещается изменять данные класса
 
 	// Возвращает площадь квадрата
 	// S = side^2
-	float getSquare() const {
-		return powf(this->side, 2);
-	}
+	float getSquare() const;
 
-	float getPerimeter() const {
-		return this->side * 4;
-	}
+	// Возвращает периметр квадрата
+	// P = 4 * side
+	float getPerimeter() const;
 
 	// Меняет значение side при условии, что указанное значение > 0
 	// Возвращает true, если параметр удовлетворяет условию
-	bool setSide(float pside) {
-		// Если pside меньше 0, то возвращаем false
-		if (pside < 0) return false;
-		// Иначе параметр указан корректно
-		else {
-			this->side = pside;
-			return true;
-		}
-	}
+	bool setSide(float pside);
 };
+
+
+// Конструктор 
+// Инициализирует значение side, если указанное значение больше или равно 0
+// Иначе задает по умолчанию 0
+Square::Square(float pside) {
+	// Если pside меньше 0, то задаем значение по умолчанию
+	if (pside < 0) this->side = 0;
+	// Иначе параметр указан корректно
+	else this->side = pside;
+}
+
+// Возвращает значение стороны
+float Square::getSide() const {
+	return this->side;
+}
+// const здесь означает, что у функции ограничение на использование 
+// То есть если функция объявлена с константным указателем this,
+// то в теле функции запрещается изменять данные класса
+
+// Возвращает площадь квадрата
+// S = side^2
+float Square::getSquare() const {
+	return powf(this->side, 2);
+}
+
+// Возвращает периметр квадрата
+// P = 4 * side
+float Square::getPerimeter() const {
+	return this->side * 4;
+}
+
+// Меняет значение side при условии, что указанное значение > 0
+// Возвращает true, если параметр удовлетворяет условию
+bool Square::setSide(float pside) {
+	// Если pside меньше 0, то возвращаем false
+	if (pside < 0) return false;
+	// Иначе параметр указан корректно
+	else {
+		this->side = pside;
+		return true;
+	}
+}
