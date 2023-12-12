fa7fa955ddf9dc97e825cffb71e5e35b003eba6f
ZaninV3 <zanin03.03@mail.ru>
2023-12-12 17:48:47 +0900

    Создан класс Square и открыт проект geometric figures
    Для начала в классе Square реализованы сеттеры, геттеры и конструктор

---
+++ geometric figures/Classes Shapes/Classes Shapes/Classes Shapes.cpp
@@ -0,0 +1,11 @@
+﻿#include "square.h"  // Поключение заголовочного файла с классом Square
+#include <iostream>  // Ввод/вывод в консоль
+
+// Точка входа и выхода из программы
+int main() {
+	Square test_square = Square();
+	std::cout << test_square.getSide() << std::endl;
+	
+	std::cout << "trying to change value..." << test_square.setSide(5.9f) << std::endl;
+	std::cout << test_square.getSide() << std::endl;
+}
